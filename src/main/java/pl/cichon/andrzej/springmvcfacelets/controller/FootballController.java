e607029149302c1ff90d8fd00566c0b178eb8fe3
陈宇峰 <yufengcode@gmail.com>
2017-03-28 13:59:50 +0800

    根据url请求返回JOSN

--- src/main/java/pl/cichon/andrzej/springmvcfacelets/controller/FootballController.java
+++ src/main/java/pl/cichon/andrzej/springmvcfacelets/controller/FootballController.java
@@ -1,47 +1,32 @@
 package pl.cichon.andrzej.springmvcfacelets.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/football")
 public class FootballController {
-	
-	private List<String> players;
 
 	@RequestMapping(value="/{teamname}", method=RequestMethod.GET)
-	public ModelAndView foo(@PathVariable String teamname){
-		players = new ArrayList<String>();
-		ModelMap map = new ModelMap();
-		if("barcelona".equalsIgnoreCase(teamname)){
-			players.add("Lionel Messi");
-			players.add("Andres Iniesta");
-			players.add("David Villa");
-			players.add("Dani Alves");
-			map.addAttribute("team", "FC Barcelona");
-		}
-		else if("juventus".equalsIgnoreCase(teamname)){
-			players.add("Andrea Pirlo");
-			players.add("Claudio Marchisio");
-			players.add("Arturo Vidal");
-			players.add("Andrea Barzagli");
-			map.addAttribute("team", "Juventus");
-		}
-		else if("manutd".equalsIgnoreCase(teamname)){
-			players.add("Wayne Rooney");
-			players.add("Rio Ferdinand");
-			players.add("Robin Van Persie");
-			players.add("Shinji Kagawa");
-			map.addAttribute("team", "Manchester United");
-		}		
-		map.addAttribute("players", players);		
-		return new ModelAndView("football_players", map);
+    @ResponseBody public String foo(@PathVariable String teamname){
+
+	    //组装JSON返回
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", "Jack");
+        jsonObject.put("age", 12);
+
+        JSONObject jsonObject1 = new JSONObject();
+        jsonObject1.put("name", "Mary");
+
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.add(jsonObject);
+        jsonArray.add(jsonObject1);
+
+        return jsonArray.toString();
 	}
 }
