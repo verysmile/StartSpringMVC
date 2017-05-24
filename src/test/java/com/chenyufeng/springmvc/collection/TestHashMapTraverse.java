68dc0f43704f5b75738ff5dd26e838af29f4a4a9
陈宇峰 <yufengcode@gmail.com>
2017-05-24 19:09:58 +0800

    HashMap-List的键值对删除

--- src/test/java/com/chenyufeng/springmvc/collection/TestHashMapTraverse.java
+++ src/test/java/com/chenyufeng/springmvc/collection/TestHashMapTraverse.java
@@ -2,7 +2,9 @@ package com.chenyufeng.springmvc.collection;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -28,5 +30,47 @@ public class TestHashMapTraverse {
         for (String s : map.values()) {
             System.out.println(s);
         }
+    }
+
+    @Test
+    public void testHashMap_List() {
+
+        String aaa = "aaa";
+        String bbb = "bbb";
+
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        List<String> list1 = new ArrayList<String>();
+        list1.add(aaa);
+
+        List<String> list2 = new ArrayList<String>();
+        list2.add(bbb);
+
+        map.put("1", list1);
+        map.put("2", list2);
+
+        //移除list中的值，最后虽然list为空，但是键还是存在的，值是一个空的列表
+        list1.remove(aaa);
+
+        for (String s : map.keySet()) {
+            System.out.println("键：" + s);
+        }
+
+        for (List<String> list : map.values()) {
+            System.out.println("值：" + list);
+        }
+
+        System.out.println("=======================");
+
+        //删除键为1的键值对
+        map.remove("1");
+
+        for (String s : map.keySet()) {
+            System.out.println("键：" + s);
+        }
+
+        for (List<String> list : map.values()) {
+            System.out.println("值：" + list);
+        }
+
     }
 }
