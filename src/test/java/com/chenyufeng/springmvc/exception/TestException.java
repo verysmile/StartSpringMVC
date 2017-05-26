9d013310f6376c9514e87e35e20c7e5f35739a00
陈宇峰 <yufengcode@gmail.com>
2017-05-26 19:35:22 +0800

    Exception抛出异常

---
+++ src/test/java/com/chenyufeng/springmvc/exception/TestException.java
@@ -0,0 +1,51 @@
+package com.chenyufeng.springmvc.exception;
+
+import org.junit.Test;
+
+/**
+ * by chenyufeng on 2017/5/26 .
+ */
+public class TestException {
+
+    /**
+     * 构造某些方法的异常，需要通知外部
+     * @param a
+     * @throws Exception
+     */
+    public void testBaseParams(int a) throws Exception {
+        if(a == 0) {
+            throw new Exception("输入参数不能为0");
+        }
+    }
+
+    /**
+     * 调用某个抛出异常的方法，可以自己不处理，继续向外抛出
+     * @throws Exception
+     */
+    public void testCallBaseparams() throws Exception {
+        testBaseParams(0);
+    }
+
+    /**
+     * 正确处理了抛出的异常，单测能通过
+     */
+    @Test
+    public void testExcep1() {
+        try {
+            testCallBaseparams();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * 没有处理异常，所以就会抛出异常；
+     * 导致单测不能通过
+     * @throws Exception
+     */
+    @Test
+    public void testExcep2() throws Exception {
+        testCallBaseparams();
+    }
+}
