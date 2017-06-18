3fe417735897bd0c06a6ba86fbe6f172d1a4d044
陈宇峰 <yufengcode@gmail.com>
2017-06-18 15:05:31 +0800

    servlet:MyServlet

---
+++ src/main/java/com/chenyufeng/snippet/MyServlet.java
@@ -0,0 +1,20 @@
+package com.chenyufeng.snippet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * by chenyufeng on 2017/6/18 .
+ */
+public class MyServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().append("my first servlet");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doGet(request, response);
+    }
+}
